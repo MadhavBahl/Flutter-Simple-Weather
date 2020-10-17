e99113c7a0ac07adb168f88d788bc6cbf38f1dbd
MadhavBahlMD <madhavbahl10@gmail.com>
2020-10-17 15:45:11 +0530

    Integrate location

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kLinearGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Colors.purple, Colors.blue],
+);
